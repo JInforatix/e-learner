3f21e7c0237e6c46e6168bde97b7dabf85ae8ad9
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-02-18 14:25:07 +0100

    Initial commit

---
+++ src/main/java/com/jinforatix/elerner/ELernerApplication.java
@@ -0,0 +1,13 @@
+package com.jinforatix.elerner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELernerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELernerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jinforatix/elerner/ELernerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinforatix.elerner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELernerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
