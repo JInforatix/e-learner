0045c0e31489d0769817df790610ad7e8c79ac91
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-03-04 13:11:07 +0100

    Add @Temporal annotations to JPA entity for instant fields
    I added the @Temporal annotation to the relevant fields in the
    JPA entity to ensure that timestamp values are correctly mapped to the database.
     This will improve the accuracy and consistency of date-related
    queries and operations on this entity.

--- src/main/java/com/jinforatix/app/coursemanager/domain/model/Course.java
+++ src/main/java/com/jinforatix/app/coursemanager/domain/model/Course.java
@@ -3,6 +3,9 @@ package com.jinforatix.app.coursemanager.domain.model;
 import java.math.BigDecimal;
 import java.time.Instant;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -10,6 +13,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 public class Course {
@@ -28,20 +33,21 @@ public class Course {
 
     private BigDecimal price;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
     private Instant creationDate;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(updatable = false)
     private Instant lastModified;
 
     public Course() {}
 
-    public Course(Author author, String title, String description, BigDecimal price, Instant creationDate,
-            Instant lastModified) {
+    public Course(Author author, String title, String description, BigDecimal price) {
         this.author = author;
         this.title = title;
         this.description = description;
         this.price = price;
-        this.creationDate = creationDate;
-        this.lastModified = lastModified;
     }
 
     public Long getId() {
@@ -92,8 +98,5 @@ public class Course {
         return lastModified;
     }
 
-    public void setLastModified(Instant lastModified) {
-        this.lastModified = lastModified;
-    }
 
 }
